a89afef5459d7056e01ffe357610526b935342c0
lbch22 <lucas.bouche@student.uclouvain.be>
2021-03-15 14:18:37 +0100

    Création de recette

--- app/src/main/java/com/example/keskonmange/FillInCreate.java
+++ app/src/main/java/com/example/keskonmange/FillInCreate.java
@@ -1,27 +1,40 @@
 package com.example.keskonmange;
-
-import android.content.Intent;
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.basgeekball.awesomevalidation.AwesomeValidation;
 import com.basgeekball.awesomevalidation.ValidationStyle;
 import com.basgeekball.awesomevalidation.utility.RegexTemplate;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.SetOptions;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class FillInCreate extends AppCompatActivity {
+    private static final String TAG = "FillInCreate";
+
+    private static final String KEY_TITLE = "nom" ;
+    private static final String KEY_INGREDIENT = "ingredient" ;
+    private static final String KEY_DESCRIPTION = "description" ;
+
+
     // Initialize variable
-    EditText etnom_recette, etingredient, etdescription;
-    Button btSubmit;
-    AwesomeValidation awesomeValidation;
-    Recipe recipe;
-    DatabaseReference reff;
+    private EditText etnom_recette, etingredient, etdescription;
+    private FirebaseFirestore reff = FirebaseFirestore.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,54 +45,35 @@ public class FillInCreate extends AppCompatActivity {
         etnom_recette = findViewById(R.id.nom_recette);
         etingredient = findViewById(R.id.ingredient);
         etdescription = findViewById(R.id.description);
-
-        // Assign variable button
-        btSubmit = findViewById(R.id.submit_recipe_creator);
-
-        //Assign Variable database
-        recipe = new Recipe();
-        reff = FirebaseDatabase.getInstance().getReference().child("Recipes");
-
-        // Initialize Validation style
-
-        awesomeValidation = new AwesomeValidation(ValidationStyle.BASIC);
-        // Add Validation for Name
-        awesomeValidation.addValidation(this, R.id.nom_recette,
-                RegexTemplate.NOT_EMPTY,R.string.invalid_name);
-        // Add Validation for Ingredient
-        awesomeValidation.addValidation(this, R.id.ingredient,
-                RegexTemplate.NOT_EMPTY,R.string.invalid_ingredient);
-        // Add Validation
-        awesomeValidation.addValidation(this, R.id.description,
-                RegexTemplate.NOT_EMPTY,R.string.invalid_recipe_description);
-
-
-        btSubmit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //check validation
-                if (awesomeValidation.validate()){
-                    // set recipe's attributes
-                    recipe.setRecette(etnom_recette.getText().toString().trim());
-                    recipe.setIngredient(etingredient.getText().toString().trim());
-                    recipe.setDescription((etdescription.getText().toString().trim()));
-
-                    // database update
-                    reff.child(recipe.getRecette()).setValue(recipe);
-
-                    Toast.makeText(getApplicationContext()
-                            ,"Recette enregistrée", Toast.LENGTH_SHORT).show();
-                    Intent intent = new Intent(FillInCreate.this, CreationOrConsulationPage.class);
-                    startActivity(intent);
-                } else {
-                    Toast.makeText(getApplicationContext()
-                            , "Il y a eu un problème d'encodage", Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
-
     }
 
-}
+        public void SaveRecette(View v) {
+                String nom  = etnom_recette.getText().toString();
+                String ingredient = etingredient.getText().toString();
+                String description  = etdescription.getText().toString();
+
+                Map<String, Object> note = new HashMap<>() ;
+                note.put(KEY_TITLE, nom);
+                note.put(KEY_INGREDIENT, ingredient);
+                note.put(KEY_DESCRIPTION, description);
+
+                    // database update
+                reff.collection("Recette").document(nom).set(note, SetOptions.merge())
+                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void aVoid) {
+                                Toast.makeText(FillInCreate.this,"Recette enregistrée", Toast.LENGTH_SHORT).show();
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(FillInCreate.this, "Il y a eu un problème d'encodage", Toast.LENGTH_SHORT).show();
+                                Log.d(TAG,e.toString());
+                            }
+                        }) ;
+        }
+    };
+
 
 
--- app/src/main/java/com/example/keskonmange/Recettes.java
+++ app/src/main/java/com/example/keskonmange/Recettes.java
@@ -5,7 +5,20 @@ public class Recettes {
     private String Ingredient1;
     private String Ingredient2;
     private String Ingredient3;
+    /*private String Ingredient;*/
     private String Description;
+
+    /*public void setTitre(String titre) {
+        Titre = titre;
+    }
+
+    public void setIngredient(String ingredient) {
+        Ingredient = ingredient;
+    }
+
+    public void setDescription(String description) {
+        Description = description;
+    }*/
 
     public Recettes(){ // Obligatoire de crée un object vide pour que ca fonctionne
 
@@ -21,12 +34,8 @@ public class Recettes {
     }
 
 
-    public String getTitre(){
-        return Titre;
-    }
-    public String getDescription(){
-        return Description;
-    }
+    public String getTitre(){ return Titre; }
+    public String getDescription(){ return Description; }
     public String getIngredient1() {return Ingredient1;}
     public String getIngredient2() {return Ingredient2;}
     public String getIngredient3() {return Ingredient3;}
