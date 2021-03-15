42fade4823fb4254c3bf2e55462405c3f9919d10
adrienkinart <adrien.kinart@student.uclouvain.be>
2021-03-15 10:06:20 +0100

    Fonctionnalité pour aller rechercher les données du profil. Il faut encore seulement être capable de le faire afficher sur l'application à l'endroit que l'on souhaite.

--- app/src/main/java/com/example/keskonmange/AuthentificatorApp.java
+++ app/src/main/java/com/example/keskonmange/AuthentificatorApp.java
@@ -1,14 +1,55 @@
 package com.example.keskonmange;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
 
 public class AuthentificatorApp extends AppCompatActivity {
+
+    TextView fullName, email;
+
+    FirebaseAuth fAuth;
+    FirebaseFirestore fstore;
+
+    String userId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_authentificator_app);
+        fullName = findViewById(R.id.textViewProfileName);
+        email= findViewById(R.id.textViewProfileEmail);
+
+        fAuth = FirebaseAuth.getInstance();
+        fstore = FirebaseFirestore.getInstance();
+
+        userId = fAuth.getCurrentUser().getUid();
+        // retrieve the data from the DB
+        DocumentReference documentReference = fstore.collection("Users").document(userId);
+        documentReference.addSnapshotListener(this, new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot documentSnapshot, @Nullable FirebaseFirestoreException error) {
+                fullName.setText(documentSnapshot.getString("FullName")); // attention: same key as in Register class
+                email.setText(documentSnapshot.getString("Email"));
+            }
+        });
+    }
+
+
+    public void LogOut(View view) {
+        FirebaseAuth.getInstance().signOut(); //logout
+        startActivity(new Intent(getApplicationContext(), Login.class));
+        finish();
     }
 }
