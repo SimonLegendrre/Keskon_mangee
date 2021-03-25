0aff14d4b3e7274c459fbe418ab0c2dc6a4e8d60
viclawford <78651844+viclawford@users.noreply.github.com>
2021-03-25 17:01:39 +0100

    Modifications FillInCreate Luca et Vic - 25/03/2021

--- app/src/main/java/com/example/keskonmange/FillInCreate.java
+++ app/src/main/java/com/example/keskonmange/FillInCreate.java
@@ -3,13 +3,17 @@ package com.example.keskonmange;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -19,6 +23,11 @@ public class FillInCreate extends OptionsMenuActivity {
     private EditText editTextTitre;
     private EditText editTextIngredients;
     private EditText editTextDescription;
+    public Button buttonAjouter;
+    public Button buttonSupprimer;
+    ArrayList<String> ListeIngredients;
+    ArrayAdapter<String> arrayAdapterListeIngredients;
+    ListView listView;
 
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
     private CollectionReference AllRecipe = db.collection("Recette");
@@ -39,13 +48,47 @@ public class FillInCreate extends OptionsMenuActivity {
         editTextDescription = findViewById(R.id.description);
         editTextTitre = findViewById(R.id.nom_recette);
         editTextIngredients = findViewById(R.id.ingredients);
-
+        buttonAjouter = (Button) findViewById(R.id.btn_ajouter);
+        buttonSupprimer = (Button) findViewById(R.id.btn_supprimer);
+        listView = findViewById(R.id.list_ingredients);
         // Ce code permet de rajouter l'ID de l'utilisateur qui crée la recette au champ de la recette
         // retrieve the data from the DB
         DocumentReference documentReference = fstore.collection("Users").document(userId);
         fstore = FirebaseFirestore.getInstance();
         userId = fAuth.getCurrentUser().getUid();
 
+
+        ListeIngredients = new ArrayList<>();
+        // fait le lien entre le XML EditText et arrayList "ingredientList"
+        arrayAdapterListeIngredients = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1, ListeIngredients);
+
+        //Bouton pour ajouter un ingrédient dans la recette
+        buttonAjouter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // stock  les Strings
+                String strIngredient = editTextIngredients.getText().toString();
+                // on ajouter le editText format String dans le ArrayList
+                ListeIngredients.add(strIngredient);
+                // on update arrayAdapter
+                listView.setAdapter(arrayAdapterListeIngredients);
+                // on update Listview grace à ArrayAdapter
+                arrayAdapterListeIngredients.notifyDataSetChanged();
+                // on vide EditText
+                editTextIngredients.getText().clear();
+            }
+        });
+        //Bouton pour supprimer le dernier ingrédient qui a été entré.
+        buttonSupprimer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ListeIngredients.remove(ListeIngredients.size() - 1);
+                listView.setAdapter(arrayAdapterListeIngredients);
+                arrayAdapterListeIngredients.notifyDataSetChanged();
+            }
+        });
         // FIN
 
 
@@ -56,18 +99,10 @@ public class FillInCreate extends OptionsMenuActivity {
         // Adding Recipe
         String titre = editTextTitre.getText().toString();
         String description = editTextDescription.getText().toString();
-        String ingredient = editTextIngredients.getText().toString();
         String userID = userId;
-        String[] list_ingredient = ingredient.split("\\s*;\\s*");
-        Arrays.sort(list_ingredient);
-        List<String> ingredients = Arrays.asList(list_ingredient);
-        Recettes recette = new Recettes(titre, description,userID, ingredients); // User ID ajouté pour ajouter l'ID utilisatuer
+        Recettes recette = new Recettes(titre, description,userID, ListeIngredients); // User ID ajouté pour ajouter l'ID utilisatuer
         AllRecipe.add(recette);
 
-
-        /*
-        @TODO : finaliser en incorporant ce qui est marqué sur Trello
-         */
 
 
         // Rediriger vers le menu lorsque l'on clique
