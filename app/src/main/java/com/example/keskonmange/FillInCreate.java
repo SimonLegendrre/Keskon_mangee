8f0dd04a61b85e5e0251536362916201cc171382
adrienkinart <adrien.kinart@student.uclouvain.be>
2021-04-06 20:27:35 +0200

    AutoComplete dans la section 'Ingredient' si celui-ci est inclus dans la base de données.

--- app/src/main/java/com/example/keskonmange/FillInCreate.java
+++ app/src/main/java/com/example/keskonmange/FillInCreate.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
@@ -15,7 +16,11 @@ import com.basgeekball.awesomevalidation.utility.RegexTemplate;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
 
@@ -23,7 +28,9 @@ public class  FillInCreate extends OptionsMenuActivity {
 
 
     private EditText editTextTitre;
-    private EditText editTextIngredients;
+
+    private AutoCompleteTextView AtcIngredients;
+
     private EditText editTextDescription;
     public Button buttonAjouter;
     public Button buttonSupprimer;
@@ -42,6 +49,11 @@ public class  FillInCreate extends OptionsMenuActivity {
     AwesomeValidation awesomeValidationIngredients;
 
 
+    // Création de BDD nécessaire (vide pour le moment) pour l'autcomplétion.
+    private static String [] IngredientsKKM = new String[]{};
+    ArrayList<String> IngredientsAajouter = new ArrayList<>();
+    private CollectionReference IngredientsKKMCollection = db.collection("IngredientsKKM");
+
 
 
 
@@ -50,9 +62,39 @@ public class  FillInCreate extends OptionsMenuActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fill_in_create);
 
+        // Importation de la BDD incluant tous les ingrédients de KKM (sert à approvisioner l'array IngredientsKKM
+        IngredientsKKMCollection
+                .addSnapshotListener(this, new EventListener<QuerySnapshot>() {
+                            @Override
+                            public void onEvent(QuerySnapshot queryDocumentSnapshots, FirebaseFirestoreException e) {
+                                if (e != null) {
+                                    return;
+                                }
+
+                                for (QueryDocumentSnapshot documentSnapshot : queryDocumentSnapshots) {
+                                    //Recettes recette = documentSnapshot.toObject(Recettes.class);
+                                    //recette.setDocumentId(documentSnapshot.getId());
+
+                                    IngredientsAajouter.add((String) documentSnapshot.get("Nom"));
+                                }
+                                // Créer l'array à partir de l'arraylist
+                                IngredientsKKM = IngredientsAajouter.toArray(new String[0]);
+                                /* code pour vérifier si la BDD est complète.
+                                for (int i=0; i<IngredientsKKM.length; i++){
+                                    System.out.println(IngredientsKKM[i]);
+                                }
+                                 */
+                            }
+                        });
+
         editTextDescription = findViewById(R.id.description);
         editTextTitre = findViewById(R.id.nom_recette);
-        editTextIngredients = findViewById(R.id.ingredients);
+
+        //Les ingredients (format = autocomplete)
+        AtcIngredients = findViewById(R.id.ingredients);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, IngredientsKKM);
+        AtcIngredients.setAdapter(adapter);
+
         buttonAjouter = (Button) findViewById(R.id.btn_ajouter);
         buttonSupprimer = (Button) findViewById(R.id.btn_supprimer);
         listView = findViewById(R.id.list_ingredients);
@@ -83,7 +125,7 @@ public class  FillInCreate extends OptionsMenuActivity {
                 if(awesomeValidationIngredients.validate()) {
 
                     // stock  les Strings
-                    String strIngredient = editTextIngredients.getText().toString();
+                    String strIngredient = AtcIngredients.getText().toString();
                     // on ajouter le editText format String dans le ArrayList
                     ListeIngredients.add(strIngredient);
                     // on update arrayAdapter
@@ -91,7 +133,7 @@ public class  FillInCreate extends OptionsMenuActivity {
                     // on update Listview grace à ArrayAdapter
                     arrayAdapterListeIngredients.notifyDataSetChanged();
                     // on vide EditText
-                    editTextIngredients.getText().clear();
+                    AtcIngredients.getText().clear();
                 }
                 else{return;}
 
