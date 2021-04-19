0104bb095e6f6183c0c4d6b08cd26c15cb469ae7
adrienkinart <adrien.kinart@student.uclouvain.be>
2021-04-19 12:37:06 +0200

    Lors de la consultation de toutes les recettes (Recipes_Scrolling.java), les notes sont affichées en dessous des recettes.

--- app/src/main/java/com/example/keskonmange/Recipes_Scrolling.java
+++ app/src/main/java/com/example/keskonmange/Recipes_Scrolling.java
@@ -1,11 +1,20 @@
 package com.example.keskonmange;
 
+import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
@@ -21,7 +30,8 @@ import java.util.ArrayList;
 public class Recipes_Scrolling extends OptionsMenuActivity {
 
     private static final String TAG = "Choix_recipe_consult";
-    private ListView listViewAlldata;
+    //private ListView listViewAlldata;
+    private ListView ListViewNameAndNote;
 
     // Initialisation base de données
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
@@ -32,19 +42,31 @@ public class Recipes_Scrolling extends OptionsMenuActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipes_scrolling);
-        listViewAlldata = findViewById(R.id.list_all_recipes);
+        //listViewAlldata = findViewById(R.id.list_all_recipes);
+
+        // Arraylist avec les notes:
+        ArrayList<String> NoteLists = new ArrayList<String>();
 
         // Définition des ArrayList et de l'adapteur : l'adapteur permet de transformer un ArrayList en ListView dans le XML.
         ArrayList<String> recipes_list = new ArrayList<>();
         ArrayList<String> recipes_list_id = new ArrayList<>();
-        ArrayAdapter adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, recipes_list);
+        //ArrayAdapter adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, recipes_list);
+
+
+        // Empty list : Titre et Rating bar element
+        ListViewNameAndNote = (ListView) findViewById(R.id.list_all_recipes);
+        //HashMap<String, RatingBar> NameAndRatingBar = new HashMap<>();
+
+
 
         // String que l'on va Intent dans DetailDescription, pour savoir l'origine de l'action. Voir explication dans l'activité
         // "AuthentificatorApp", ligne 122 ou "Choice_recipe_consult" ligne 53
         String Recipe_scrolling_acti = "Scrolling";
 
+
         // Afficher toutes les recettes en listView
         AllRecipe.addSnapshotListener(this, new EventListener<QuerySnapshot>() {
+            @RequiresApi(api = Build.VERSION_CODES.O)
             @Override
             public void onEvent(QuerySnapshot queryDocumentSnapshots, FirebaseFirestoreException e) {
                 if (e != null) {
@@ -58,20 +80,50 @@ public class Recipes_Scrolling extends OptionsMenuActivity {
                     Recettes recette = documentSnapshot.toObject(Recettes.class);
                     recette.setDocumentId(documentSnapshot.getId());
                     String documentId = recette.getDocumentId();
+
+                    //Aggrégation des notes moyennes afin de les afficher dans la listView
+                    //RatingBar ratingBar = (RatingBar) findViewById(R.id.RecipeRatingBarRecipeScrolling);
+
+                    if(recette.getNote() != null && !recette.getNote().isNaN()) {
+                        System.out.println(recette.getNote());
+                        System.out.println("Il y a une note");
+                        float note = recette.getNote().floatValue();
+                        //ratingBar.setRating(note);
+                        NoteLists.add("Note moyenne: "+note+"/5");
+                        System.out.println("Je répète, Il y a une note");
+                        //NameAndRatingBar.put(recette.getName(), ratingBar);
+                    }
+                    else{ // pour le moment, quand il n'y a pas de rating, elle vaut zéro
+                        System.out.println("Il n'y a pas encore de note");
+                        // ratingBar.setRating((float) 0.0);
+                        NoteLists.add("Pas encore notée");
+                        //NameAndRatingBar.put(recette.getName(), ratingBar);
+                        System.out.println("Je répète, Il n'y a pas de note");
+                    }
+
+
                     titre = recette.getName();
                     id_recipe = recette.getDocumentId();
                     DocumentReference document = db.collection("Recettes").document(id_recipe);
                     recipes_list.add(titre);
                     recipes_list_id.add(id_recipe);
-
                 }
 
 
-                // ListView
-                listViewAlldata.setAdapter(adapter);
+
+                MyAdapter adapter_bis = new MyAdapter(Recipes_Scrolling.this, recipes_list, NoteLists );
+                ListViewNameAndNote.setAdapter(adapter_bis);
+
+                System.out.println("liste du chien: \n"+ String.join("\n", recipes_list) );
+
+
+
+                // ListView ==> ça a changé apres
+                //listViewAlldata.setAdapter(adapter);
 
                 // Action lorsque l'on clique sur l'une des recettes
-                listViewAlldata.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+                ListViewNameAndNote.setOnItemClickListener(new AdapterView.OnItemClickListener() {
                     @Override
                     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                         Intent intent = new Intent(Recipes_Scrolling.this, DetailedDescription.class);
@@ -87,7 +139,38 @@ public class Recipes_Scrolling extends OptionsMenuActivity {
         });
 
 
+
     }
+
+    class MyAdapter extends ArrayAdapter<String>{
+        Context context ;
+        ArrayList<String> rTitre ;
+        ArrayList<String> rRating;
+
+        MyAdapter(Context c, ArrayList<String> title, ArrayList<String> rating){
+            super(c, R.layout.recipes_items_to_display, R.id.TextViewRecipeTitleRecipeScrolling, title);
+            this.context = c;
+            this.rTitre = title;
+            this.rRating = rating;
+        }
+
+
+        @NonNull
+        @Override
+        public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+            LayoutInflater layoutInflater = (LayoutInflater) getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View recipes_items = layoutInflater.inflate(R.layout.recipes_items_to_display, parent, false);
+            TextView myTitle = recipes_items.findViewById(R.id.TextViewRecipeTitleRecipeScrolling);
+            TextView myNote = recipes_items.findViewById(R.id.TextViewRecipesNote);
+
+            // now set our resources on views
+            myTitle.setText(rTitre.get(position));
+            myNote.setText(rRating.get(position));
+
+            return recipes_items;
+        }
+    }
+
 
 }
 
